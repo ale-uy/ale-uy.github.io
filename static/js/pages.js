6903a940a6a8683b991ee81a97e09e9b79826b82
Ale <46233654+ale-uy@users.noreply.github.com>
2023-05-01 18:03:58 -0300

    Update pages.js

--- static/js/pages.js
+++ static/js/pages.js
@@ -1,27 +1,29 @@
 // Obtener elementos de la lista y los proyectos
 const projectList = document.querySelector('.project-list');
 const projects = document.querySelectorAll('.project');
+const projectDescription = document.querySelector('.project-description');
 
 // Función para mostrar un proyecto específico y ocultar los demás
 function showProject(projectId) {
-	projects.forEach(project => {
-		if (project.id === projectId) {
-			project.classList.remove('hidden');
-		} else {
-			project.classList.add('hidden');
-		}
-	});
+  projects.forEach(project => {
+    if (project.id === projectId) {
+      project.classList.remove('hidden');
+      projectDescription.innerHTML = project.dataset.description;
+    } else {
+      project.classList.add('hidden');
+    }
+  });
 }
 
 // Añadir evento click a los elementos de la lista
 projectList.addEventListener('click', e => {
-	const target = e.target;
-	if (target.tagName === 'LI') {
-		const projectId = target.getAttribute('data-project');
-		showProject(projectId);
+  const target = e.target;
+  if (target.tagName === 'LI') {
+    const projectId = target.getAttribute('data-project');
+    showProject(projectId);
 
-		// Cambiar la selección actual en la lista
-		projectList.querySelector('.selected').classList.remove('selected');
-		projectList.querySelector([data-project="${projectId}"]).classList.add('selected');
-	}
+    // Cambiar la selección actual en la lista
+    projectList.querySelector('.selected').classList.remove('selected');
+    target.classList.add('selected');
+  }
 });
