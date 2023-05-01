3ed3399bdce1de9993534cda4dad59b0977197a9
Ale <46233654+ale-uy@users.noreply.github.com>
2023-05-01 13:49:20 -0300

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,1 @@
-const imageList = document.querySelector('.image-list');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
 
-prevButton.addEventListener('click', () => {
-  imageList.scrollLeft -= 100;
-});
-
-nextButton.addEventListener('click', () => {
-  imageList.scrollLeft += 100;
-});
