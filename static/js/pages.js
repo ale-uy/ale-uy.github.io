2f5b2972a656d0205d386f594a6deec1cef7974a
Ale <46233654+ale-uy@users.noreply.github.com>
2023-05-01 18:13:59 -0300

    Update pages.js

--- static/js/pages.js
+++ static/js/pages.js
@@ -1,29 +1,43 @@
 // Obtener elementos de la lista y los proyectos
 const projectList = document.querySelector('.project-list');
 const projects = document.querySelectorAll('.project');
-const projectDescription = document.querySelector('.project-description');
 
 // Función para mostrar un proyecto específico y ocultar los demás
 function showProject(projectId) {
-  projects.forEach(project => {
-    if (project.id === projectId) {
-      project.classList.remove('hidden');
-      projectDescription.innerHTML = project.dataset.description;
-    } else {
-      project.classList.add('hidden');
-    }
-  });
+	projects.forEach(project => {
+		if (project.id === projectId) {
+			project.classList.remove('hidden');
+		} else {
+			project.classList.add('hidden');
+		}
+	});
+}
+
+// Función para seleccionar un proyecto de la lista
+function selectProject(selectedItem) {
+	// Remover la clase 'selected' de todos los elementos de la lista
+	projectList.querySelectorAll('li').forEach(item => {
+		item.classList.remove('selected');
+	});
+
+	// Añadir la clase 'selected' al elemento seleccionado
+	selectedItem.classList.add('selected');
+
+	// Obtener el ID del proyecto seleccionado
+	const projectId = selectedItem.getAttribute('data-project');
+
+	// Mostrar el proyecto seleccionado en la columna derecha
+	showProject(projectId);
 }
 
 // Añadir evento click a los elementos de la lista
 projectList.addEventListener('click', e => {
-  const target = e.target;
-  if (target.tagName === 'LI') {
-    const projectId = target.getAttribute('data-project');
-    showProject(projectId);
-
-    // Cambiar la selección actual en la lista
-    projectList.querySelector('.selected').classList.remove('selected');
-    target.classList.add('selected');
-  }
+	const target = e.target;
+	if (target.tagName === 'LI') {
+		selectProject(target);
+	}
 });
+
+// Seleccionar el primer proyecto de la lista al cargar la página
+selectProject(projectList.querySelector('li'));
+
