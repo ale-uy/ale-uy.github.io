2dbc3ae28b39b4cbd6e79d8dcb65831e8119e649
ale-uy <lasm1984@gmail.com>
2025-09-19 17:08:01 -0300

    a

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Necesario para el despliegue en la raíz de ale-uy.github.io
+  base: '/',
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000,
+  },
 });
