161b196b4d7ab567f21ba38724efa301f4add0e8
ale-uy <lasm1984@gmail.com>
2025-09-19 15:07:12 -0300

    Init background

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/', // Necesario para el despliegue en la raíz de ale-uy.github.io
+});
